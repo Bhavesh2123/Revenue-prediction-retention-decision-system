2cffacb4bee504b796735b4ef454baad9231ca09
Bhavesh2123 <146112682+Bhavesh2123@users.noreply.github.com>
2026-02-22 03:49:58 +0530

    revenue traning done

---
+++ train_revenue_model.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import joblib
+from src.data_preprocessing import(
+    load_data,
+    add_total_price,
+    time_based_split
+)
+from src.feature_engineering import(
+    build_rfm,
+    build_additional_features,
+    build_Future_Revenue,
+    merge_all_features
+)
+
+from src.ltv_model import train_model
+#Load data
+df=load_data("Raw_Data.csv")
+df= add_total_price(df)
+
+cutoff_date=pd.Timestamp("2011-06-30")
+prediction_end= pd.Timestamp("2011-12-31")
+
+#Time Splitted
+past_data, future_data= time_based_split(df, cutoff_date, prediction_end)
+
+rfm= build_rfm(past_data, cutoff_date)
+extra_features = build_additional_features(past_data, cutoff_date)
+future_revenue= build_Future_Revenue(future_data)
+
+model_df= merge_all_features(rfm, extra_features, future_revenue)
+
+model= train_model(model_df)
+joblib.dump(model, "models/revenue_model.pkl")
+print("Training Complete.")
+
+
