1a4af57ca0068744f28e6d10ded286825df7981b
Bhavesh2123 <146112682+Bhavesh2123@users.noreply.github.com>
2026-02-24 22:33:50 +0530

    churn model updated

--- train_churn_model.py
+++ train_churn_model.py
@@ -13,3 +13,13 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, roc_auc_score
 
+df= load_data(Dataset/Raw_Data.csv)
+df = add_total_price(df)
+
+cutoff_date= pd.Timestamp("2011-06-30")
+churn_window_end = pd.Timestamp("2011-09-30")
+
+past_data, future_data= time_based_split(
+    df, cutoff_date, pd.Timestamp("2011-12-31")
+)
+
