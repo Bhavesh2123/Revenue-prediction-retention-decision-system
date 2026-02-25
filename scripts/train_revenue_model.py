7fb8e1e771910cbd53a297596ef89c7a98ff090e
Bhavesh2123 <146112682+Bhavesh2123@users.noreply.github.com>
2026-02-25 13:07:56 +0530

    -

--- scripts/train_revenue_model.py
+++ scripts/train_revenue_model.py
@@ -18,23 +18,25 @@ from src.feature_engineering import(
 
 from src.ltv_model import train_model
 #Load data
-df=load_data("Dataset/Raw_Data.csv")
-df= add_total_price(df)
+def run_rvenue_training():
+    df=load_data("Dataset/Raw_Data.csv")
+    df= add_total_price(df)
 
-cutoff_date=pd.Timestamp("2011-06-30")
-prediction_end= pd.Timestamp("2011-12-31")
+    cutoff_date=pd.Timestamp("2011-06-30")
+    prediction_end= pd.Timestamp("2011-12-31")
 
 #Time Splitted
-past_data, future_data= time_based_split(df, cutoff_date, prediction_end)
+    past_data, future_data= time_based_split(df, cutoff_date, prediction_end)
 
-rfm= build_rfm(past_data, cutoff_date)
-extra_features = build_additional_features(past_data, cutoff_date)
-future_revenue= build_Future_Revenue(future_data)
+    rfm= build_rfm(past_data, cutoff_date)
+    extra_features = build_additional_features(past_data, cutoff_date)
+    future_revenue= build_Future_Revenue(future_data)
 
-model_df= merge_all_features(rfm, extra_features, future_revenue)
+    model_df= merge_all_features(rfm, extra_features, future_revenue)
 
-model= train_model(model_df)
-joblib.dump(model, "scripts/revenue_model.pkl")
-print("Training Complete.")
+    model= train_model(model_df)
+    joblib.dump(model, "scripts/revenue_model.pkl")
+    print("Revenue training Complete.")
+    return model
 
 
