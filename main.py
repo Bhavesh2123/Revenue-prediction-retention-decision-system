dd8447641f85f6c8b54f26f95dd198ee6bc14580
Bhavesh2123 <146112682+Bhavesh2123@users.noreply.github.com>
2026-02-25 12:47:26 +0530

    main.py updated

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from train_revenue_model import *
-from train_churn_model import *
+from scripts.train_revenue_model import run_revenue_training
+from scripts.train_churn_model import run_churn_training
 
-print("All models trained successfully.")
+
+if __name__ == "__main__":
+    run_revenue_training()
+    run_churn_training()
+    print("All models trained successfully.")
