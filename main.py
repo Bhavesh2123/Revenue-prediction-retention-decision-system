5d7b3760f3d60372f3f60291ea12362eca05a4ec
Bhavesh2123 <146112682+Bhavesh2123@users.noreply.github.com>
2026-02-25 17:24:13 +0530

    All problems resolved

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from scripts.train_churn_model import run_churn_training
 if __name__ == "__main__":
     run_revenue_training()
     run_churn_training()
+
     print("All models trained successfully.")
