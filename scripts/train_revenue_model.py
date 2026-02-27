3a7bdb55d33efbc7e60aa692d604d33734a82c68
Bhavesh2123 <146112682+Bhavesh2123@users.noreply.github.com>
2026-02-27 15:43:59 +0530

    path fixed

--- scripts/train_revenue_model.py
+++ scripts/train_revenue_model.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import pandas as pd
@@ -19,7 +21,7 @@ from src.feature_engineering import(
 from src.ltv_model import train_model
 #Load data
 def run_revenue_training():
-    df=load_data("Dataset/Raw_Data.csv")
+    df=load_data(BASE_DIR / "Dataset" / "Raw_Data.csv")
     df= add_total_price(df)
 
     cutoff_date=pd.Timestamp("2011-06-30")
@@ -35,7 +37,7 @@ def run_revenue_training():
     model_df= merge_all_features(rfm, extra_features, future_revenue)
 
     model= train_model(model_df)
-    joblib.dump(model, "scripts/revenue_model.pkl")
+    joblib.dump(model, BASE_DIR / "models" / "revenue_model.pkl")
     print("Revenue training Complete.")
     return model
 
